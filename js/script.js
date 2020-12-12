111f235ec64ebf1ce8e05011a1b8f5f4090387ca
Alexey Blokhin <bl0khin.alexey@yandex.ru>
2020-12-12 15:29:40 +0500

    variables fix

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ let container = document.querySelector('.popup');
 let editBtn = document.querySelector('.profile__edit-btn');
 let closeBtn = document.querySelector('.popup__close-btn');
 let formSave = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name-field');
-let aboutInput = document.querySelector('.popup__about-field');
+let namePopup = document.querySelector('.popup__name-field');
+let aboutPopup = document.querySelector('.popup__about-field');
 let nameProfile = document.querySelector('.profile__name');
 let aboutProfile = document.querySelector('.profile__about');
 
 function openPopup() {
     container.classList.add('popup_opened');
 
-    nameInput.value = nameProfile.textContent;
-    aboutInput.value = aboutProfile.textContent;
+    namePopup.value = nameProfile.textContent;
+    aboutPopup.value = aboutProfile.textContent;
 }
 
 function closePopup() {
@@ -21,8 +21,8 @@ function closePopup() {
 function formSubmitHandler (evt) {
     evt.preventDefault();
     
-    nameProfile.textContent = nameInput.value;
-    aboutProfile.textContent = aboutInput.value;
+    nameProfile.textContent = namePopup.value;
+    aboutProfile.textContent = aboutpopup.value;
 
     container.classList.remove('popup_opened');
 }
