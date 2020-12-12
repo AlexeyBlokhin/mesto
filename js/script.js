596ad58c4b78de28628882e51e8ec16d0ef8fe98
Alexey Blokhin <bl0khin.alexey@yandex.ru>
2020-12-12 15:27:23 +0500

    js fix

--- js/script.js
+++ js/script.js
@@ -6,37 +6,26 @@ let nameInput = document.querySelector('.popup__name-field');
 let aboutInput = document.querySelector('.popup__about-field');
 let nameProfile = document.querySelector('.profile__name');
 let aboutProfile = document.querySelector('.profile__about');
-let likeBtn = document.querySelectorAll('.elements__like-btn');
 
 function openPopup() {
     container.classList.add('popup_opened');
 
-    nameInput.placeholder = nameProfile.textContent;
-    aboutInput.placeholder = aboutProfile.textContent;
-}
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    
-    nameInput.getAttribute('value');
-    aboutInput.getAttribute('value');
-       
-    nameProfile.textContent = nameInput.value;
-    aboutProfile.textContent = aboutInput.value;
-
-    container.classList.remove('popup_opened');
+    nameInput.value = nameProfile.textContent;
+    aboutInput.value = aboutProfile.textContent;
 }
 
 function closePopup() {
     container.classList.remove('popup_opened');
 }
 
-function likeFunc(e) {
-    e.target.classList.toggle('elements__like-btn_active');
-}
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    
+    nameProfile.textContent = nameInput.value;
+    aboutProfile.textContent = aboutInput.value;
 
-for(let i=0; i<likeBtn.length; i++)
-	likeBtn[i].addEventListener("click",likeFunc);
+    container.classList.remove('popup_opened');
+}
 
 editBtn.addEventListener('click', openPopup);
 closeBtn.addEventListener('click', closePopup);
