1d7244ec7dd91403e9a698d33fdd518d5314a1ff
Alexey Blokhin <bl0khin.alexey@yandex.ru>
2020-12-19 02:51:36 +0500

    add form done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-const container = document.querySelector('.popup');
-const editBtn = document.querySelector('.profile__edit-btn');
-const closeBtn = document.querySelector('.popup__close-btn');
-const formSave = document.querySelector('.popup__form');
-const namePopup = document.querySelector('.popup__input_content_name');
-const aboutPopup = document.querySelector('.popup__input_content_about');
-const nameProfile = document.querySelector('.profile__name');
-const aboutProfile = document.querySelector('.profile__about');
+const popupBlock = document.querySelector('.popup'); //блок popup целиком
+const popupContainer = document.querySelector('.popup__container'); //общий елемент контейнеров форм/полноразмерных изображений
+const closeBtns = document.querySelectorAll('.popup__close-btn'); //псевдомассив кнопок закрытия
+const nameProfile = document.querySelector('.profile__name'); //имя профиля
+const aboutProfile = document.querySelector('.profile__about'); //род деятельности профиля
 
+//форма редактирования профиля
+const editPopup = document.querySelector('.popup__container_type_edit'); //контейнер формы редактированя
+const editBtn = document.querySelector('.profile__edit-btn'); //кнопка вызова формы редактирования
+const nameEditForm = document.querySelector('.popup__input_content_name'); //поле ввода имени
+const aboutEditForm = document.querySelector('.popup__input_content_about'); //поле ввода рода деятельности
+
+//форма добавления нового изображения
+const addPopup = document.querySelector('.popup__container_type_add'); //контейнер формы добавления
+const addBtn = document.querySelector('.profile__add-btn'); //кнопка вызова формы добавления
+const nameAddForm = document.querySelector('.popup__input_content_image-name');
+const linkAddForm = document.querySelector('.popup__input_content_image-link');
+
+const formSave = document.querySelector('.popup__form'); //кнопка submit
 
 const initialCards = [
     {
@@ -38,13 +48,14 @@ const initialCards = [
 const listContainerElement = document.querySelector('.elements__grid');
 const templateElement = document.querySelector('.template');
 
+//добавляет созданные из массива места в DOM
 function renderList() {
     const listItems =  initialCards.map(composeItem);
 
     listContainerElement.append(...listItems);
     
 }
-    
+//создает элементы списка из массива    
 function composeItem({name, link}) {
     const newItem = templateElement.content.cloneNode(true);
     const cardTitle = newItem.querySelector(".mesto__title");
@@ -53,33 +64,60 @@ function composeItem({name, link}) {
     cardTitle.textContent = name;
     cardImage.src = link;
 
-
     return newItem;
 }
 
-renderList();
+//показывает блок popup
+function showPopupBlock() {
+    popupBlock.classList.add('popup_opened');
+    console.log('блок есть');
+};
 
-
-function openPopup() {
-    container.classList.add('popup_opened');
-
-    namePopup.value = nameProfile.textContent;
-    aboutPopup.value = aboutProfile.textContent;
+//скрывает блок popup
+function hidePopupBlock() {
+    popupBlock.classList.remove('popup_opened');
+    console.log('блока нет');
 }
 
-function closePopup() {
-    container.classList.remove('popup_opened');
-}
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
+//показывает форму/полноразмерное изображение
+function showForm(a) {
     
-    nameProfile.textContent = namePopup.value;
-    aboutProfile.textContent = aboutPopup.value;
+    nameEditForm.value = nameProfile.textContent;
+    aboutEditForm.value = aboutProfile.textContent;
+    
+    a.classList.add('popup_opened');
 
-    closePopup();
+    showPopupBlock();
+};
+
+//закрывает форму/полноразмерное изображение
+function closePopup(a) {
+    a.classList.remove('popup_opened');
+
+    hidePopupBlock();
 }
 
-editBtn.addEventListener('click', openPopup);
-closeBtn.addEventListener('click', closePopup);
-formSave.addEventListener('submit', formSubmitHandler);
+//добавляет новое изображение
+function addNewItem() {
+    const inputName = nameAddForm.value;
+    const inputLink = linkAddForm.value;
+    const newItem = composeItem({name: inputName, link: inputLink});
+    listContainerElement.prepend(newItem);
+    nameAddForm.value = '';
+    linkAddForm.value = '';
+}
+
+//лисенер кнопки редактирования профиля
+editBtn.addEventListener('click', function() {showForm(editPopup)});
+
+//лисенер кнопки добавление изображений
+addBtn.addEventListener('click', function() {showForm(addPopup)});
+
+//лисенер кнопок закрытия
+[...closeBtns].forEach(function(closeBtns) {
+    closeBtns.addEventListener('click', function(evt) {
+        closePopup(evt.target.closest('.popup__container'));
+    });
+});
+
+renderList();
